7aec658c5dc5e30a271e42ea6e19f19fe11084ac
Jen Moriarty <JMoriarty78@gmail.com>
2023-04-23 13:15:05 -0600

    Update mild chatbot.py

--- mild chatbot.py
+++ mild chatbot.py
@@ -14,9 +14,15 @@ while doExit == False:
         print("I'm sorry to hear you're feeling that way.")
     elif choice.find("happy") != -1 or choice.find("glad") != -1 or choice.find("excited") != -1 or choice.find("looking forward") != -1 :
         print("That's great!")
+    elif choice.find("I'm") != -1: #these next three lines let you repeat the word after "I'm" back in a sentence
+        word_list = choice.split() #break the sentence into a list with one word per slot
+        next_word = word_list[word_list.index("I'm")+1] #find the word after "I'm"
+        print("Why are you", next_word+ "?") #repeat it back 
+        #NOTE: it would be good to add an if statement here checking if the next word was "a" 
+        #so if someone says "I'm a frog", it doesn't say back, "Why are you a?"
 
     #listen and respond to specific topics
-    if choice.find("mom") != -1 or choice.find("dad") != -1 or choice.find("brother") != -1 or choice.find("sister") != -1 :
+    elif choice.find("mom") != -1 or choice.find("dad") != -1 or choice.find("brother") != -1 or choice.find("sister") != -1 :
         print("Tell me more about your family.")
 
     elif choice.find("dog") != -1 or choice.find("cat") != -1 or choice.find("fish") != -1 or choice.find("bird") != -1 :
