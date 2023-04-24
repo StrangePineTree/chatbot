1864378a3688909d078603be08ced7ce3c945d75
StrangePineTree <97989279+StrangePineTree@users.noreply.github.com>
2023-04-24 14:01:31 -0600

    Update mild chatbot.py

--- mild chatbot.py
+++ mild chatbot.py
@@ -20,11 +20,20 @@ while doExit == False:
         print("Why are you", next_word+ "?") #repeat it back 
         #NOTE: it would be good to add an if statement here checking if the next word was "a" 
         #so if someone says "I'm a frog", it doesn't say back, "Why are you a?"
-
+    if choice.find("burgers") != -1 or choice.find("pizza") != -1 or choice.find("ramen") != -1 or choice.find("crepes"):
+        print("i like that food too.")
     #listen and respond to specific topics
-    elif choice.find("mom") != -1 or choice.find("dad") != -1 or choice.find("brother") != -1 or choice.find("sister") != -1 :
+    elif   choice.find("dad") != -1 or choice.find("brother") != -1 or choice.find("sister") != -1 :
         print("Tell me more about your family.")
+    elif choice.find("java") != -1:
+        print("i see you are part of the javascript fandom")
+    elif choice.find ("mom"):
+        num = random.randrange(1,2)
+        if num == 1:
+            print ("hey siri tell me a yo mama joke")
 
+        else:
+            print("yo momma so fat she made a bean and cheese burrito scared")
     elif choice.find("dog") != -1 or choice.find("cat") != -1 or choice.find("fish") != -1 or choice.find("bird") != -1 :
         print("I'd like to hear more about this pet.")
 
